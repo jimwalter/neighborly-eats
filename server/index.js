3732574d00d32c644ce05a41f14c47af87590a2f
jim walter <jimwalter13090@gmail.com>
2020-04-25 14:50:38 -0600

    server PORT env var

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const yelp = require('../db/seeds/generation.js');
 
