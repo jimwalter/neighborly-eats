faa89d5af1d9fa5d2d53f63e80d69352601a76d2
jim walter <jimwalter13090@gmail.com>
2020-05-19 11:28:20 -0600

    updated form

--- client/src/components/Recommendations.jsx
+++ client/src/components/Recommendations.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Restaurant from './Restaurant.jsx';
 
 const Recommendations = (props) => {
-  console.log(props.class);
   return (
     <section className="grid">
       <div className="recommendations">
